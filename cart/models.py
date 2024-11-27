cd575b08c3ff0af334ad570a7df9ac69001e6fa0
Rashedul_Islam <rashedul16801@gmail.com>
2024-11-27 23:04:23 +0600

    Pagination added

--- cart/models.py
+++ cart/models.py
@@ -14,7 +14,7 @@ class Cart(models.Model):
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
     medicine = models.ForeignKey('store.Medicine', on_delete=models.CASCADE)  # Corrected app reference
-    quantity = models.PositiveIntegerField(default=1)
+    quantity = models.PositiveIntegerField(default=0)
 
     def get_total_price(self):
         return self.medicine.price * self.quantity
--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Medicine,Category
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 def home(request):
     medicines = Medicine.objects.all()  # Retrieve all medicines
@@ -11,6 +12,16 @@ def home(request):
 
 def medicine_list(request):
     medicines = Medicine.objects.all()
+    paginator = Paginator(medicines, 24)  # 24 medicines per page
+
+    page = request.GET.get('page')
+    try:
+        medicines = paginator.page(page)
+    except PageNotAnInteger:
+        medicines = paginator.page(1)
+    except EmptyPage:
+        medicines = paginator.page(paginator.num_pages)
+
     return render(request, 'medicine_list.html', {'medicines': medicines})
 
 def medicine_detail(request, medicine_id):
@@ -24,13 +35,16 @@ def search_results(request):
     return render(request, 'search_results.html', {'medicines': results, 'query': query})
 
 def category_products(request, category_name):
-    # Fetch the category by name
     category = get_object_or_404(Category, name=category_name)
-
-    # Fetch medicines in the category
-    medicines = Medicine.objects.filter(category=category)
+    medicines_list = Medicine.objects.filter(category=category)
+    
+    # Pagination logic
+    paginator = Paginator(medicines_list, 24)  # 24 items per page
+    page_number = request.GET.get('page')  # Fetch 'page' from URL
+    medicines = paginator.get_page(page_number)
 
     return render(request, 'category_products.html', {
         'category': category,
+        'category_name': category_name,
         'medicines': medicines,
     })
